b9325f2f594d5ca1d52f37c713ad4aa0229f61ba
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-09 13:51:46 -0700

    lets gerrit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - says whether a number is + or -
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int last_digit = n % 10;
+
+	if (last_digit > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5", n, last_digit)
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %i is %i and is 0", n, last_digit)
+	}
+	else if (last_digit < 6)
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0", n, last_digit)
+	}
+	return (0);
+}
