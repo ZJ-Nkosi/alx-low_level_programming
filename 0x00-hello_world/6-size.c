1ed16ccf3a87e5767b0e49be602e52c18d37c94f
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-09 12:06:47 -0700

    six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long l;
 	float m;
 
-	printf("Size of a char: %c byte(s)\n", (int)sizeof(i));
+	printf("Size of a char: %c byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a int: %i byte(s)\n", sizeof(j));
 	printf("Size of a long int: %li byte(s)\n", sizeof(k));
-	printf("Size of a long long int: %lli byte(s)\n", sizeof(l));
-	printf("Size of a float: %f byte(s)\n", (int)sizeof(m));
+	printf("Size of a long long int: %lli byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a float: %f byte(s)\n", (unsigned long)sizeof(m));
 
 	return (0);
 }
