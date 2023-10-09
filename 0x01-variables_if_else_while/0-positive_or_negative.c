d415309ad7d3d904f9be714a4aab50ebf3bbbcc0
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-09 13:31:31 -0700

    lets gerrit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,14 +6,20 @@
  * main - says whether a number is + or -
  * Return: 0
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
 	}
 	else
 	{
