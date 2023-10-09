8e81320869bb3fecae27d96ba5a291a28008fc79
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-09 11:16:12 -0700

    this is getting hard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/**
+ * this is the first official c program
+ * main - prints a string
+ * return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
