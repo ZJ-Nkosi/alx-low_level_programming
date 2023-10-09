e06c8f2f16556dcc23782a7e31d062f714a50f63
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-09 15:03:23 -0700

    lets gerrit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,18 @@
 int main(void)
 {
 	int i = 48;
+	int j = 0;
 
 	while (i <= 57)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (j < 7)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		i++;
+		j++;
 	}
 	putchar('\n');
 	return (0);
