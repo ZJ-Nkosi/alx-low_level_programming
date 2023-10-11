3dc0cf8b1651e41460c1a7020b4a10a59cffd641
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-11 05:31:54 -0700

    two

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_islower - checks if letter is lowercase
+ */
+
+int _islower(int c)
+{
+	int i;
+	i = _islower(c);
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
