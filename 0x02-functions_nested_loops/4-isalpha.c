a41cd006b6eb1e0e2696448fd6480c487f280602
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-11 06:43:36 -0700

    three

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,13 +5,14 @@
  * @c: the character
  * Return: 1 or 0
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
