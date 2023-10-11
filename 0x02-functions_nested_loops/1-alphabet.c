537c814003de6a8be2bc4677e3568fbc0bd27b19
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-11 02:29:10 -0700

    one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - printing the alphabet with only putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 97;
+	while(i <= 122)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+	return (0);
+
+
+}
