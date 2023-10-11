6ebf9bbafd78f518a16ce29282afdb52741396f9
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-11 05:39:27 -0700

    three

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,14 +6,12 @@
 
 int _islower(int c)
 {
-	int i;
-	i = islower(c);
-	if (i == 1)
-	{
-		return (1);
-	}
-	else
+	if (c <= 65 && c >= 90)
 	{
 		return (0);
+	}
+	else if (c <= 97 && c >= 122)
+	{
+		return (1);
 	}
 }
