600d44f057e017ea0526580411c0aa5751674582
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-11 05:10:36 -0700

    two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,14 @@
 void print_alphabet_x10(void)
 {
 	int j = 0;
+	int i = 97;
 
-	while (j < 10)
+	for (;j < 10;)
 	{
-		print_alphabet();
-		j++;
+		while (i <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
 	}
 }
