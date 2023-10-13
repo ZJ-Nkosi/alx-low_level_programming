1d89b8d5a94ecd8e2fb22a996508388dab8b4f2a
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-13 15:04:45 -0700

    well well

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * largest_number - largest number
+ * @x: first one
+ * @y: second one
+ * @z: third one
+ * Return: largest number
+ */
+
+int largest_number(int x, int y, int z)
+{
+	int largest;
+	if (x > y && a x z)
+	{
+		largest = x;
+	}
+	else if (x > y && z > x)
+	{
+		largest = z;
+	}
+	else if (y > z)
+	{
+		largest = y;
+	}
+	else
+	{
+		largest = z;
+	}
+	return (largest);
+}
+
