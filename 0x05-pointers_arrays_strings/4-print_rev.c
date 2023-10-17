7fbaf3e7f2819cd4dc6accbdddbdeb6204b9250e
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-17 14:45:07 -0700

    four

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses a string
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++
+	}
+
+	j = i;
+
+	while (j != 0)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+	_putchar('\n');
+
+}
