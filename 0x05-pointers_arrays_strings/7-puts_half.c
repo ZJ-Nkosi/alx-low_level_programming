fe71a9a3676ea9af7c8bea8a3f8760210a18a078
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-17 16:02:32 -0700

    seven

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints galf a string
+ * @str: the string
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int k;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	k = i / 2;
+
+	while (str[j] > k)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
