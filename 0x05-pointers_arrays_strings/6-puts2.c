0671e55e39016dad754ec06a2c77603f0df3b3b6
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-17 15:32:26 -0700

    four

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - prints second char
+ * @str: the string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
