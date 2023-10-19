63fcc8fc0a1bd7482570099a8e70a2ed1883bcab
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-19 06:13:06 -0700

    zero

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rev_string
+ * rev_string - reverses a string
  * @s: the string
  */
 
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates 2 strings
+ * @*dest: first string
+ * @*src: second string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char concatenated = *dest + *src;
+	return (concatenated);
+}
