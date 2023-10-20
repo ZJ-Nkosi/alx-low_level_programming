bfbef341d3ba5113f6cf6d6c1700e845e576709a
Zwelethu Jr. Nkosi <zozewjnr@gmail.com>
2023-10-20 13:23:18 -0700

    two

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies
+ * @dest: argument one
+ * @src: argument two
+ * @n: number
+ * Return: a char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\n'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
